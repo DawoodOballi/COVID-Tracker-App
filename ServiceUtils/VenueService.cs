303d7ce7e0ea52a2b9fc3d75dd5567921443dc02
dawood oballi <d4w00d@hotmail.com>
2020-10-28 16:37:57 +0000

    Added VenueHelper methods

---
+++ ServiceUtils/VenueService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.ServiceUtils
+{
+    public class VenueService
+    {
+        public readonly CovidTrackerContext _context;
+
+        public VenueService(CovidTrackerContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddNewVenueAsync(Venue venue)
+        {
+            _context.Venues.Add(venue);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task RemoveUserAsync(Venue venue)
+        {
+            _context.Venues.Remove(venue);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
