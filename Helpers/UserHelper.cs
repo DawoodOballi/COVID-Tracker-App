7ea1818161194c38da52e52088fdf0a8f74ef99d
Sam Haimes <samhaimes@hotmail.com>
2020-10-28 16:37:57 +0000

    Added UserHelper with IsUserCheckedIn, CheckUserIn, CheckUserOut

--- Helpers/UserHelper.cs
+++ Helpers/UserHelper.cs
@@ -1,11 +1,61 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Exchange.WebServices.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
+using WebApplication1.Models;
 
 namespace WebApplication1.Helpers
 {
     public class UserHelper
     {
+        CovidTrackerContext _db;
+        UserHelper userHelper;
+
+        public UserHelper(CovidTrackerContext db)
+        {
+            _db = db;
+        }
+
+        public bool IsUserCheckedIn(int SelectedUserId)
+        {
+            var selectedUserWithCheckin = _db.Users.Where(x => x.UserID == SelectedUserId).Include(x => x.CheckIns).ToString();
+            if (selectedUserWithCheckin != null)
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        public void CheckUserIn(int SelectedUserId, int SelectedVenueId)
+        {
+            _db.UserCheckIns.Add(
+                new UserCheckin
+                {
+                    UserID = SelectedUserId,
+                    CheckedInAt = DateTime.Now,
+                    VenueID = SelectedVenueId
+                }
+                );
+            _db.SaveChanges();
+        }
+
+        public void CheckUserOut(int SelectedUserId, int SelectedVenueId)
+        {
+            _db.UserCheckIns.Update(
+                    new UserCheckin
+                    {
+                        UserID = SelectedUserId,
+                        CheckedOutAt = DateTime.Now,
+                        VenueID = SelectedVenueId
+                    }
+                    );
+            _db.SaveChanges();
+
+        }
+
     }
 }
