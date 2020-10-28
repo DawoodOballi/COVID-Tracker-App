42b73f29325e07e8c0f3ae730b5446df10be6d9f
Daniel Layland <laylandwork@gmail.com>
2020-10-28 12:26:15 +0000

    Added DB context, entities and models

---
+++ Migrations/20201028122431_initial-setup.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication1.Migrations
+{
+    public partial class initialsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(nullable: false),
+                    LastName = table.Column<string>(nullable: false),
+                    Title = table.Column<string>(nullable: false),
+                    DOB = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Venues",
+                columns: table => new
+                {
+                    VenueID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    Capacity = table.Column<long>(nullable: false),
+                    Add1 = table.Column<string>(nullable: false),
+                    Add2 = table.Column<string>(nullable: false),
+                    City = table.Column<string>(nullable: false),
+                    Town = table.Column<string>(nullable: true),
+                    County = table.Column<string>(nullable: false),
+                    PostCode = table.Column<string>(nullable: false),
+                    ContactNumber = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Venues", x => x.VenueID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserCheckIns",
+                columns: table => new
+                {
+                    UserCheckinID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserID = table.Column<int>(nullable: false),
+                    VenueID = table.Column<int>(nullable: false),
+                    CheckedInAt = table.Column<DateTime>(nullable: false),
+                    CheckedOutAt = table.Column<DateTime>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserCheckIns", x => x.UserCheckinID);
+                    table.ForeignKey(
+                        name: "FK_UserCheckIns_Users_UserID",
+                        column: x => x.UserID,
+                        principalTable: "Users",
+                        principalColumn: "UserID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserCheckIns_Venues_VenueID",
+                        column: x => x.VenueID,
+                        principalTable: "Venues",
+                        principalColumn: "VenueID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserCheckIns_UserID",
+                table: "UserCheckIns",
+                column: "UserID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserCheckIns_VenueID",
+                table: "UserCheckIns",
+                column: "VenueID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserCheckIns");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Venues");
+        }
+    }
+}
---
+++ Models/CovidTrackerContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class CovidTrackerContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Venue> Venues { get; set; }
+        public DbSet<UserCheckin> UserCheckIns { get; set; }
+        public CovidTrackerContext() : base(new DbContextOptionsBuilder().UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=CovidTracker;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False").Options)
+        {
+        }
+        public CovidTrackerContext(DbContextOptions options) : base (options)
+        {
+
+        }
+    }
+}
