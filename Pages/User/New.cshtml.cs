4a2a9e3d08048b348f5447f2918f069818d62997
Daniel Layland <laylandwork@gmail.com>
2020-10-28 13:52:37 +0000

    milestone commit, all working pre-validation removal

---
+++ Pages/User/New.cshtml.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApplication1.Models;
+using WebApplication1.ServiceUtils;
+
+namespace WebApplication1.Pages.User
+{
+    public class NewModel : PageModel
+    {
+        UserService _uService;
+
+        [BindProperty]
+        public Models.User NewUser { get; set; }
+        public NewModel(CovidTrackerContext db)
+        {
+            _uService = new UserService(db);
+        }
+        public void OnGet()
+        {
+
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (NewUser.Title == "Choose...")
+            {
+                ModelState.AddModelError("Title", "Please select a Title");
+                return Page();
+            }
+            if (ModelState.IsValid)
+            {
+                await _uService.AddNewUserAsync(NewUser);
+                return new JsonResult(new { success = true });
+            }
+            return Page();
+        }
+    }
+}
---
+++ ServiceUtils/UserService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.ServiceUtils
+{
+    public class UserService
+    {
+        CovidTrackerContext _db;
+        public UserService(CovidTrackerContext db)
+        {
+            _db = db;
+        }
+        public void AddNewUser(User user)
+        {
+            _db.Users.Add(user);
+            _db.SaveChanges();
+        }
+        public async Task AddNewUserAsync(User user)
+        {
+            _db.Users.Add(user);
+            await _db.SaveChangesAsync();
+        }
+    }
+}
