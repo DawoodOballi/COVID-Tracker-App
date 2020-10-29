a6e0562bcfe223211e5fe0e7fa7245124c5b94cd
Daniel Layland <laylandwork@gmail.com>
2020-10-29 10:14:44 +0000

    Did some helper method refactoring to separate out a service implementation and interface

--- ServiceUtils/UserService.cs
+++ ServiceUtils/UserService.cs
@@ -1,13 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations.Operations;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebApplication1.Models;
+using WebApplication1.ServiceUtils.Interfaces;
 
 namespace WebApplication1.ServiceUtils
 {
-    public class UserService
+    public class UserService : IUserService
     {
         CovidTrackerContext _db;
         public UserService(CovidTrackerContext db)
@@ -23,6 +25,21 @@ namespace WebApplication1.ServiceUtils
         {
             _db.Users.Add(user);
             await _db.SaveChangesAsync();
+        }
+        public User GetUserByID(int id)
+        {
+            return _db.Users.Where(x => x.UserID == id).Include(x => x.CheckIns).FirstOrDefault();
+        }
+        public void AddUserCheckin(UserCheckin checkin)
+        {
+            _db.UserCheckIns.Add(checkin);
+            _db.SaveChanges();
+        }
+        public void CheckOut(int CheckInId, DateTime checkoutTime)
+        {
+            var record = _db.UserCheckIns.Where(x => x.CheckedOutAt == null && x.UserCheckinID == CheckInId).FirstOrDefault();
+            record.CheckedOutAt = checkoutTime;
+            _db.SaveChanges();
         }
     }
 }
